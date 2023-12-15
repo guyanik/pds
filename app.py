5eba22dd7bc5f2794bc252c5435b7234ddb9f5e6
guyanik <68607184+guyanik@users.noreply.github.com>
2023-12-15 03:26:29 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -160,15 +160,15 @@ with tab2:
 
     if timeframe == "Weekly WO":
         # first day of the week
-        df = df.groupby("LOCUS").resample("W").sum().round(2).reset_index()
-        df_total = df_total.resample("W").sum().round(2).reset_index()
+        df = df.groupby("LOCUS").resample("W").sum().round(2).reset_index(drop=True)
+        df_total = df_total.resample("W").sum().round(2).reset_index(drop=True)
     elif timeframe == "Monthly WO":
         # first day of the month
-        df = df.groupby("LOCUS").resample("MS").sum().round(2).reset_index()
-        df_total = df_total.resample("MS").sum().round(2).reset_index()
+        df = df.groupby("LOCUS").resample("MS").sum().round(2).reset_index(drop=True)
+        df_total = df_total.resample("MS").sum().round(2).reset_index(drop=True)
     elif timeframe == "Yearly WO":
-        df = df.groupby("LOCUS").resample("YS").sum().round(2).reset_index()
-        df_total = df_total.resample("YS").sum().round(2).reset_index()
+        df = df.groupby("LOCUS").resample("YS").sum().round(2).reset_index(drop=True)
+        df_total = df_total.resample("YS").sum().round(2).reset_index(drop=True)
     
     wo1 = st.empty()
 
