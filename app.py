23dd8c675eb5f68c48b6ed886ad641c23407313f
guyanik <68607184+guyanik@users.noreply.github.com>
2023-12-15 03:47:26 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -50,6 +50,8 @@ with tab1:
                             max_value=pd.to_datetime("2023-12-31"), 
                             value=df.index[-1])
 
+        print(date)
+        print(df.index.to_list())
         if date in df.index.to_list():
             # create columns
             seconds_spent, user_count, quantity, volume, weight, price = st.columns(6)
