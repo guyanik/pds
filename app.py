4a8ba28dd61fab670401453b0262f89edc51a668
guyanik <68607184+guyanik@users.noreply.github.com>
2023-12-15 04:47:26 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -231,15 +231,15 @@ with tab3:
 
     if timeframe == "Weekly WL":
         # first day of the week
-        df = df.groupby("LOCUS").resample("W").sum().round(2).reset_index(drop=True)
-        df_total = df_total.resample("W").sum().round(2).reset_index(drop=True)
+        df = df.groupby("LOCUS").resample("W").sum().round(2).drop(columns=["DATE", 'LOCUS']).reset_index()
+        df_total = df_total.resample("W").sum().round(2).drop(columns=["DATE"]).reset_index()
     elif timeframe == "Monthly WL":
         # first day of the month
-        df = df.groupby("LOCUS").resample("MS").sum().round(2).reset_index(drop=True)
-        df_total = df_total.resample("MS").sum().round(2).reset_index(drop=True)
+        df = df.groupby("LOCUS").resample("MS").sum().round(2).drop(columns=["DATE", 'LOCUS']).reset_index()
+        df_total = df_total.resample("MS").sum().round(2).drop(columns=["DATE"]).reset_index()
     elif timeframe == "Yearly WL":
-        df = df.groupby("LOCUS").resample("YS").sum().round(2).reset_index(drop=True)
-        df_total = df_total.resample("YS").sum().round(2).reset_index(drop=True)
+        df = df.groupby("LOCUS").resample("YS").sum().round(2).drop(columns=["DATE", 'LOCUS']).reset_index()
+        df_total = df_total.resample("YS").sum().round(2).drop(columns=["DATE"]).reset_index()
     
     wl1 = st.empty()
 
